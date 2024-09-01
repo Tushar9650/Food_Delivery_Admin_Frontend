53926961f51e4a2030877601cd67add267c6e302
tushar <tusharsinghota@gmail.com>
2024-09-01 18:26:40 +0530

    update backend links

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url = "http://localhost:4200";
+  const url = "https://food-delivery-backend-2-oztd.onrender.com";
   return (
     <div>
     <ToastContainer/>
